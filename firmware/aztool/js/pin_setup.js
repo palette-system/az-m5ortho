c4bce1479dedc528c5da850f9da2410965861a48
suzukiyouichi <yo1sjp@outlook.jp>
2022-04-18 04:58:24 +0900

    add serial js

---
+++ firmware/aztool/js/pin_setup.js
@@ -0,0 +1,305 @@
+
+let pinstp = {};
+
+// モーダル閉じた後のコールバック
+pinstp.cb_func = null;
+
+// 受け取ったIOエキスパンダデータ
+pinstp.ioxp_option = {};
+
+pinstp.zero = function(n, len){
+    return n.toString().padStart(len, "0");
+};
+
+// ピン設定初期化
+pinstp.init = function() {
+    // モーダル用HTML登録
+    pinstp.init_html();
+    // モーダル登録
+    pinstp.mdl = $('[data-remodal-id=pinset_modal]').remodal();
+    pinstp.mdl.settings.closeOnOutsideClick = false;
+    pinstp.mdl.settings.hashTracking = false;
+
+};
+
+// モーダル用HTML登録
+pinstp.init_html = function() {
+    let html = `
+        <div class="remodal" data-remodal-id="pinset_modal" 
+                data-remodal-options="hashTracking: false, closeOnOutsideClick: false"
+                style="max-width: 1200px;border-radius: 40px;overflow: hidden;">
+            <!-- クローズボタン -->
+            <!-- button data-remodal-action="close" class="remodal-close"></button> -->
+
+        <table>
+            <tr>
+                <td style="text-align: center; width: 500px;">
+                    <font style="font-size:24;"><b>アドレス：</b></font>　
+                    <select id="addr_select" class="addrsl" onChange="javascript:pinstp.ic_update();">
+                        <option value="0" class="addrsl">　0　</option>
+                        <option value="1" class="addrsl">　1　</option>
+                        <option value="2" class="addrsl">　2　</option>
+                        <option value="3" class="addrsl">　3　</option>
+                        <option value="4" class="addrsl">　4　</option>
+                        <option value="5" class="addrsl">　5　</option>
+                        <option value="6" class="addrsl">　6　</option>
+                        <option value="7" class="addrsl">　7　</option>
+                    </select>
+                    <br><br>
+                    <b style="font-size: 24px;">mcp23017</b><br>
+                    <table cellpadding="0" cellspacing="0" style="display: inline-block; width: 400px;">
+                        <tr><td style="height: 20px;">　</td><td rowspan="16" style="text-align: center;width: 180px;"><img src="./img/mcp23017.png"></td><td>　</td></tr>
+                        <tr><td class="pbox" id="pin_01" align="right"></td><td class="pbox" id="pin_28"></td></tr>
+                        <tr><td class="pbox" id="pin_02" align="right"></td><td class="pbox" id="pin_27"></td></tr>
+                        <tr><td class="pbox" id="pin_03" align="right"></td><td class="pbox" id="pin_26"></td></tr>
+                        <tr><td class="pbox" id="pin_04" align="right"></td><td class="pbox" id="pin_25"></td></tr>
+                        <tr><td class="pbox" id="pin_05" align="right"></td><td class="pbox" id="pin_24"></td></tr>
+                        <tr><td class="pbox" id="pin_06" align="right"></td><td class="pbox" id="pin_23"></td></tr>
+                        <tr><td class="pbox" id="pin_07" align="right"></td><td class="pbox" id="pin_22"></td></tr>
+                        <tr><td class="pbox" id="pin_08" align="right"></td><td class="pbox" id="pin_21"></td></tr>
+                        <tr><td class="pbox" id="pin_09" align="right"></td><td class="pbox" id="pin_20"></td></tr>
+                        <tr><td class="pbox" id="pin_10" align="right"></td><td class="pbox" id="pin_19"></td></tr>
+                        <tr><td class="pbox" id="pin_11" align="right"></td><td class="pbox" id="pin_18"></td></tr>
+                        <tr><td class="pbox" id="pin_12" align="right"></td><td class="pbox" id="pin_17"></td></tr>
+                        <tr><td class="pbox" id="pin_13" align="right"></td><td class="pbox" id="pin_16"></td></tr>
+                        <tr><td class="pbox" id="pin_14" align="right"></td><td class="pbox" id="pin_15"></td></tr>
+                        <tr><td>　</td><td>　</td></tr>
+                    </table>
+                    
+                </td>
+                <td style="width: 20px;"></td>
+                <td>
+                    <div id="pin_setting_right"></div>
+                    <br><br>
+                    <div style="text-align: right; width: 580px;margin:0;">
+                        <a id="pin_set_ok" class="exec-button" onClick="javascript:pinstp.eve_ok_click(1);">決定</a>　
+                        <a id="pin_set_cancel" class="exec-button" onClick="javascript:pinstp.eve_ok_click(0);">キャンセル</a>　
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>`;
+    $("body").append(html);
+    // 設定枠HTML初期化
+    pinstp.set_pin_setting_html();
+
+};
+
+// 設定枠HTML初期化
+pinstp.set_pin_setting_html = function() {
+    $("#pin_setting_right").html(`<b>Direct</b><br>
+    <ul id="list_direct" class="iopinbox sortable ioxp_pintype_box"></ul><br>
+    <b>Row</b><br>
+    <ul id="list_row" class="iopinbox sortable ioxp_pintype_box"></ul><br>
+    <b>Col</b><br>
+    <ul id="list_col" class="iopinbox sortable ioxp_pintype_box"></ul>`);
+}
+
+// ドラッグイベントの登録
+pinstp.init_sortable = function() {
+    $('.sortable').selectable({
+    cancel: '.sort-handle, .ui-selected',
+    filter: '> li'
+    }).sortable({
+    opacity: 0.9,
+    items: "> li",
+    handle: 'div, .sort-handle, .ui-selected',
+    connectWith: ".iopinbox",
+    //ヘルパー生成時（ドラッグ時）処理
+    helper: function(e, item) {
+        if ( ! item.hasClass('ui-selected') ) {
+        item.parent().children('.ui-selected').removeClass('ui-selected');
+        item.addClass('ui-selected');
+        }
+        var selected = item.parent().children('.ui-selected').clone();
+        //placeholder用の高さを取得しておく
+        ph = item.outerHeight() * selected.length;
+        item.data('multidrag', selected).siblings('.ui-selected').remove();
+        return $('<li/>').append(selected);
+    },
+    //ドラッグした時にplaceholderの高さを選択要素分取る
+    start: function(e, ui) {
+        console.log(ph);
+        console.log(ui.placeholder);
+        // ui.placeholder.css({'height':ph});
+        ui.placeholder.css({'height': 0});
+    },
+    //ドロップ時処理
+    stop: function(e, ui) {
+        var selected = ui.item.data("multidrag");
+        ui.item.after(selected);
+        ui.item.remove();
+        console.log("end");
+        selected.removeClass("ui-selected");
+        console.log(selected);
+        let sort_func = function(a, b){
+            return ((parseInt($(a).attr("id").split("_")[1]) > parseInt($(b).attr("id").split("_")[1]))? 1 : -1);
+        };
+        $("#list_direct").html($("ul#list_direct li").sort(sort_func));
+        $("#list_row").html($("ul#list_row li").sort(sort_func));
+        $("#list_col").html($("ul#list_col li").sort(sort_func));
+        pinstp.ic_update(); // ピン情報更新
+    }
+    });
+
+    pinstp.ic_update(); // ピン情報更新
+};
+
+// ピン情報の表示
+pinstp.vset = function(obj_num, set_txt, bgcolor) {
+    let obj_id = "#pin_" + obj_num;
+    let i = parseInt(obj_num);
+    let txt = "";
+    $(obj_id).css({"background-color": bgcolor});
+    if (i <= 14) {
+        txt = set_txt + "<font style='font-size: 14px;color: #666;font-weight: normal;'> ." + obj_num + "</font>";
+    } else {
+        txt = "<font style='font-size: 14px;color: #666;font-weight: normal;'>" + obj_num + ". </font>" + set_txt;
+    }
+    $(obj_id).html(txt);
+};
+
+// ピン情報の表示更新
+pinstp.ic_update = function() {
+
+    // gpioとicのピン番号のマッピング
+    let pim_map = {
+        "gpio_00": "21",
+        "gpio_01": "22",
+        "gpio_02": "23",
+        "gpio_03": "24",
+        "gpio_04": "25",
+        "gpio_05": "26",
+        "gpio_06": "27",
+        "gpio_07": "28",
+        "gpio_08": "01",
+        "gpio_09": "02",
+        "gpio_10": "03",
+        "gpio_11": "04",
+        "gpio_12": "05",
+        "gpio_13": "06",
+        "gpio_14": "07",
+        "gpio_15": "08"
+    };
+    // direct row col
+    let i, k;
+    let directs = $("#list_direct").contents();
+    let rows = $("#list_row").contents();
+    let cols = $("#list_col").contents();
+    for (i in directs) {
+        if (!directs[i].id || !pim_map[directs[i].id]) continue;
+        k = pim_map[directs[i].id];
+        pinstp.vset(k, "Direct "+i, "#e2ffd6");
+    }
+    for (i in rows) {
+        if (!rows[i].id || !pim_map[rows[i].id]) continue;
+        k = pim_map[rows[i].id];
+        pinstp.vset(k, "Row "+i, "#ffd6f6");
+    }
+    for (i in cols) {
+        if (!cols[i].id || !pim_map[cols[i].id]) continue;
+        k = pim_map[cols[i].id];
+        pinstp.vset(k, "Col "+i, "#fff8ad");
+    }
+
+    // 固定ピン
+    pinstp.vset("12", "SCK", "#d9fbfd");
+    pinstp.vset("13", "SDA", "#d9fbfd");
+    pinstp.vset("09", "VCC", "#ff8081");
+    pinstp.vset("10", "GND", "#a4c5f9");
+    pinstp.vset("11", "NC", "#e0e0e0");
+    pinstp.vset("14", "NC", "#e0e0e0");
+    pinstp.vset("18", "VCC", "#ff8081");
+    pinstp.vset("19", "NC", "#e0e0e0");
+    pinstp.vset("20", "NC", "#e0e0e0");
+
+    // アドレス
+    let addr = parseInt($("#addr_select").val());
+    let addr_pin = {"15": 0x01, "16": 0x02, "17": 0x04};
+    for (k in addr_pin) {
+        if (addr & addr_pin[k]) {
+            pinstp.vset(k, "VCC", "#ff8081");
+        } else {
+            pinstp.vset(k, "GND", "#a4c5f9");
+        }
+    }
+};
+
+pinstp.li_html = function(n) {
+    let s = pinstp.zero(n, 2);
+    if (n < 8) {
+        return `<li id="gpio_` + s + `" class="ioxp_pin_box_a"><div class="ioxp_pin_box_div">A` + n + `</div></li>`;
+    } else {
+        return `<li id="gpio_` + s + `" class="ioxp_pin_box_b"><div class="ioxp_pin_box_div">B` + n + `</div></li>`;
+    }
+};
+
+// OK、キャンセルのクリックイベント(click_type = 0 キャンセル / 1 決定)
+pinstp.eve_ok_click = function(click_type) {
+    let i, r = {};
+    // キャンセルならそのまま閉じる
+    if (click_type == 0) {
+        pinstp.cb_func(click_type, {});
+        pinstp.mdl.close();
+        return;
+    }
+    // アドレス
+    r.addr = parseInt($("#addr_select").val()) + 32;
+    // direct
+    let directs = $("#list_direct").contents();
+    r.direct = [];
+    for (i in directs) {
+        if (!directs[i].id) continue;
+        r.direct.push(parseInt(directs[i].id.split("_")[1]));
+    }
+    // row
+    let rows = $("#list_row").contents();
+    r.row = [];
+    for (i in rows) {
+        if (!rows[i].id) continue;
+        r.row.push(parseInt(rows[i].id.split("_")[1]));
+    }
+    // col
+    let cols = $("#list_col").contents();
+    r.col = [];
+    for (i in cols) {
+        if (!cols[i].id) continue;
+        r.col.push(parseInt(cols[i].id.split("_")[1]));
+    }
+    console.log(r);
+    // コールバックを実行してモーダルを閉じる
+    pinstp.cb_func(click_type, r);
+    pinstp.mdl.close();
+    return;
+
+};
+
+// IOエキスパンダ設定のモーダルを開く
+pinstp.open = function(opt, cb_func) {
+    // オプション受け取り
+    pinstp.ioxp_option = opt;
+    // コールバック受け取り
+    if (!cb_func) cb_func = function() {};
+    pinstp.cb_func = cb_func;
+    // アドレス設定
+    $("#addr_select").val(opt.addr - 32);
+    let i, h;
+    // 設定枠HTML初期化
+    pinstp.set_pin_setting_html();
+    // direct 設定
+    h = "";
+    for (i in opt.direct) h += pinstp.li_html(opt.direct[i]);
+    $("#list_direct").html(h);
+    // row 設定
+    h = "";
+    for (i in opt.row) h += pinstp.li_html(opt.row[i]);
+    $("#list_row").html(h);
+    // col 設定
+    h = "";
+    for (i in opt.col) h += pinstp.li_html(opt.col[i]);
+    $("#list_col").html(h);
+    // ドラッグ動作設定
+    pinstp.init_sortable();
+    // モーダルを開く
+    pinstp.mdl.open();
+};
